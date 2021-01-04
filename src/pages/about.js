fc951728af414fa246910318f344cf86904ac40b
hugo-chan <hugoyhc14@gmail.com>
2021-01-04 22:37:55 +0800

    fade text home page

--- src/pages/about.js
+++ src/pages/about.js
@@ -11,9 +11,11 @@ export default function About() {
             </ul>
         </header>
         <h1 style={{ whiteSpace: `nowrap` }}>About me</h1>
-        <p>I am a student at the University of Chicago pursuing a B.Sc. in Computer Science and a B.A. in Economics (June 2022). </p>
-        <p>My interests include quantitative finance, cryptocurrencies, and software development.</p>
-        <p>The majority of my programming experience has been on the backend (e.g. my proprietary trading system). Technologies I am familiar with using include Python, JavaScript, C, SQL, R, and LaTeX.</p>
+        <div>
+          <p>I am a student at the University of Chicago pursuing a B.Sc. in Computer Science and a B.A. in Economics (June 2022). </p>
+          <p>My interests include quantitative finance, cryptocurrencies, and software development.</p>
+          <p>The majority of my programming experience has been on the backend (e.g. my proprietary trading system). Technologies I am familiar with using include Python, JavaScript, C, SQL, R, and LaTeX.</p>
+        </div>
     </Layout>
   )
 }
